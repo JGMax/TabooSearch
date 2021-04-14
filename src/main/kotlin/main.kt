903cea8702002dce7a83320a77a40f7b250e6cd5
JGMax <maksus20.05@mail.com>
2021-04-14 18:57:18 +0300

    Fix

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,8 @@
 
-fun main(inputFile: String) {
+fun main() {
     /*Constants.SHORT_MEMORY_DURATION = memory.toInt()
     Constants.PENALTY_START_VALUE = penalty.toLong()*/
-    DataManager.inputFile = inputFile//"src/main/resources/input.txt"
+    DataManager.inputFile = "src/main/resources/input.txt"
     DataManager.outputFile = "src/main/resources/output.txt"
     DataManager.readData(" ")
     Vehicles.run()
