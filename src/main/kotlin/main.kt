3292b3b8e6b9850a6693866b783b962e753d5480
JGMax <maksus20.05@mail.com>
2021-04-14 18:45:15 +0300

    Fix

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 
-fun main(inputFile: String, memory: String, penalty: String) {
-    Constants.SHORT_MEMORY_DURATION = memory.toInt()
-    Constants.PENALTY_START_VALUE = penalty.toLong()
+fun main(inputFile: String) {
+    /*Constants.SHORT_MEMORY_DURATION = memory.toInt()
+    Constants.PENALTY_START_VALUE = penalty.toLong()*/
     DataManager.inputFile = inputFile//"src/main/resources/input.txt"
     DataManager.outputFile = "src/main/resources/output.txt"
     DataManager.readData(" ")
